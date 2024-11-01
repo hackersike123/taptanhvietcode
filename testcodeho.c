8e760a2b6376ded4d448b83bcbf377e545dd8934
hackersike123 <130346386+hackersike123@users.noreply.github.com>
2024-11-01 19:35:11 +0000

    oke

--- testcodeho.c
+++ testcodeho.c
@@ -2,63 +2,52 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
-typedef struct
-{
+typedef struct{
     char maSo[11];
     char hoTen[41];
     float diemTB;
-} SV;
+}SV;
 
-void nhap(SV &sv)
-{
+void nhap(SV &sv){
     getchar();
     gets(sv.maSo);
     gets(sv.hoTen);
     scanf("%f", &sv.diemTB);
 }
 
-void xuat(SV sv)
-{
+void xuat(SV sv){
     printf("%s - %s - %.2f\n", sv.maSo, sv.hoTen, sv.diemTB);
 }
 
-typedef struct Node
-{
+typedef struct Node{
     SV key;
-    struct Node *pNext;
-} NODE;
+    struct Node*pNext;
+}NODE;
 
-typedef struct List
-{
-    NODE *pHead;
-    NODE *pTail;
-} LIST;
+typedef struct List{
+    NODE*pHead;
+    NODE*pTail;
+}LIST;
 
-void Init(LIST &l)
-{
+void Init(LIST &l){
     l.pHead = NULL;
     l.pTail = NULL;
 }
 
-void InsertTail(LIST &l, NODE *p)
-{
-    if (l.pHead == NULL)
-    {
+void InsertTail(LIST &l, NODE* p){
+    if(l.pHead == NULL){
         l.pTail = p;
         l.pHead = p;
     }
-    else
-    {
+    else{
         l.pTail->pNext = p;
         l.pTail = p;
     }
 }
 
-NODE *CreateNode(SV x)
-{
+NODE* CreateNode(SV x){
     NODE *p = new NODE;
-    if (p == NULL)
-    {
+    if(p == NULL){
         printf("Khong cap phat duoc");
         exit(0);
     }
@@ -67,41 +56,34 @@ NODE *CreateNode(SV x)
     return p;
 }
 
-void nhapDS(LIST &l)
-{
+void nhapDS(LIST &l){
     SV sv;
     nhap(sv);
-    NODE *p = CreateNode(sv);
+    NODE* p = CreateNode(sv);
     InsertTail(l, p);
 }
 
-void xuatDS(LIST l)
-{
+void xuatDS(LIST l){
     char maSo[11];
     getchar();
     gets(maSo);
     bool check = false;
-    for (NODE *i = l.pHead; i != NULL; i = i->pNext)
-    {
-        if (strcmp(i->key.maSo, maSo) == 0)
-        {
+    for(NODE* i = l.pHead; i != NULL; i = i->pNext){
+        if(strcmp(i->key.maSo, maSo) == 0){
             xuat(i->key);
             check = true;
         }
     }
-    if (check == false)
-        printf("Not Found");
+    if(check == false) printf("Not Found");
 }
 
-int main()
-{
-    // Tiếp tục hoàn thiện mã nguồn
+int main() {
+    //Tiếp tục hoàn thiện mã nguồn
     int n;
     scanf("%d", &n);
     LIST l;
     Init(l);
-    for (int i = 0; i < n; i++)
-    {
+    for(int i = 0; i < n; i++){
         nhapDS(l);
     }
     xuatDS(l);
